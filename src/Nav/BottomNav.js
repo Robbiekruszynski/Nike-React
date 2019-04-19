7d27527f9a528d1952a80603a1ca5e153b18dc70
Robbie Kruszynski <robbiekruszynski@gmail.com>
2019-04-19 15:19:55 -0700

    add work to Navbar

--- src/Nav/BottomNav.js
+++ src/Nav/BottomNav.js
@@ -2,26 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 function BottomNav() {
-  const Nav = styled.div`
-  color:ready
-  `;
 
   const Box = styled.div`
   display:flex;
   flex-direction: row;
-  justify-content: flex-start;
-  border:1px solid grey;
-  height:10px;
+  background-color: lightgrey;
+  height:40px;
+  width: 100vw;
+  `;
+
+  const Text = styled.h4`
+  text-align: center;
   width: 100vw;
   `;
 
 
   return(
     <div>
-      <Nav>
-      <Box>a</Box>
-      Bottom Nav
-      </Nav>
+    <Box>
+      <Text>Extra 20% OFF ALL SALE</Text>
+    </Box>
     </div>
   )
 }
