b01142a9651f25a4ba85b67281a07a38fc98a996
Robbie Kruszynski <robbiekruszynski@gmail.com>
2019-04-25 13:24:31 -0700

    add work to navbar

--- src/Nav/TopNav.js
+++ src/Nav/TopNav.js
@@ -4,28 +4,34 @@ import styled from 'styled-components';
 function TopNav() {
   const Box = styled.div`
   display:flex;
-  flex-direction: row;
-  border:1px solid grey;
+  border:1px solid green;
   height:30px;
   width:100vw;
   `;
 
   const LeftSide = styled.button`
-  /* display:flex;
-  flex-direction: row; */
+  display:flex;
+  flex-direction: row;
   justify-content: flex-start;
   width: 70px;
-  /* border:1px solid red; */
+  border:1px solid red;
   `;
 
-  const RightSide = styled.div`
+  const RightSide = styled.button`
   display:flex;
   flex-direction:row;
   justify-content: flex-end;
-  /* border: 1px solid black; */
+  border: 1px solid blue;
   margin-right: 10px;
   `;
 
+  const End = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-left: 500px;
+  width: 100vw;
+
+  `;
   return(
     <div>
       <Box>
@@ -33,12 +39,14 @@ function TopNav() {
       <LeftSide>Jordan</LeftSide>
       <LeftSide>Hurley</LeftSide>
       <LeftSide>Converse</LeftSide>
-      <RightSide>
-      <p>Join/Login To NikePlusAccount</p>
-      <p>Help</p>
-      <p>Cart</p>
-      <p>Flag</p>
-      </RightSide>
+
+      <End>
+        <RightSide>Join/Login To NikePlusAccount</RightSide>
+        <RightSide>Help </RightSide>
+        <RightSide>Cart</RightSide>
+        <RightSide>Flag</RightSide>
+      </End>
+
       </Box>
     </div>
   )
